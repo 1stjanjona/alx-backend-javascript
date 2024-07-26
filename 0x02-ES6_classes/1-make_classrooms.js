5da837818afeda350bdbe8a5ab080031c971c3fa
1stjanjona <jannat.hammouche@outlook.com>
2024-07-26 13:01:11 +0000

    classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-	const size1 = new ClassRoom(19);
-	const size2 = new Classroom(20);
-	const size3 = new ClassRoom(34);
-	return [size1, size2, size3];
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }

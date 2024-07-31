8ae0857b86dddb6db220454541ec2bb0acfa317b
1stjanjona <jannat.hammouche@outlook.com>
2024-07-31 22:48:45 +0000

    0x04-TypeScript

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,24 @@
+/// <reference path="Teacher.ts" />
+/// <reference path="Subjects.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Java";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher.experienceTeachingJava) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
+
+export const java = new Subjects.Java();

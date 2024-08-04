f3ae72b9be6102cc194a264197e4111032cf8307
1stjanjona <jannat.hammouche@outlook.com>
2024-08-04 18:18:43 +0000

    manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+function cleanSet(set, startString) {
+    if (typeof startString !== 'string' || startString.length === 0) {
+        return '';
+    }
+    
+    const result = [];
+    
+    set.forEach(value => {
+        if (typeof value === 'string' && value.startsWith(startString)) {
+            result.push(value.slice(startString.length));
+        }
+    });
+    
+    return result.join('-');
+}
+
+export default cleanSet;
